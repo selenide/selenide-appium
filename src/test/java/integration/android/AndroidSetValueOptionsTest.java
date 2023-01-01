c29773af02cbaa14eedf53f1ffd3c36a11719fce
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2023-01-01 19:12:54 +0530

    used deeplink to launch login page

--- src/test/java/integration/android/AndroidSetValueOptionsTest.java
+++ src/test/java/integration/android/AndroidSetValueOptionsTest.java
@@ -1,25 +1,17 @@
 package integration.android;
 
-import com.codeborne.selenide.SetValueOptions;
-import com.codeborne.selenide.appium.AppiumSetValueOptions;
 import com.codeborne.selenide.appium.SelenideAppium;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.appium.AppiumSelectors.byText;
-import static io.appium.java_client.AppiumBy.xpath;
 
 class AndroidSetValueOptionsTest extends BaseSwagLabsAndroidTest {
 
   @Test
   void testHideKeyboardForAndroid() {
-    SelenideAppium.launchApp();
-
-    $(xpath("//android.view.ViewGroup[@content-desc='open menu']/android.widget.ImageView"))
-      .shouldBe(visible).click();
-    $(byText("Log In")).shouldBe(visible).click();
+    SelenideAppium.openAndroidDeepLink("mydemoapprn://login", "com.saucelabs.mydemoapp.rn");
     $(AppiumBy.accessibilityId("Username input field")).shouldBe(visible);
       //.setValue(AppiumSetValueOptions.withText("abcd123").hideKeyboard());
   }
